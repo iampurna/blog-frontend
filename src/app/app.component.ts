1da3bf03d1bec3b0174538b035ab72df6f331c6e
iampurna <linkwithpurna@gmail.com>
2024-09-11 23:56:19 +0545

    button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
